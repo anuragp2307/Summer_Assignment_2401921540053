504c6ff5169789d9d255f32ab928489f8e040e43
ANURAG PARASHAR <anuragparashar0000@gmail.com>
2026-06-10 23:31:26 +0530

    Update RepeatedSubstringPattern.java

--- week_2/day_3/RepeatedSubstringPattern.java
+++ week_2/day_3/RepeatedSubstringPattern.java
@@ -1,1 +1,7 @@
 
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        String concatenated = s + s;
+        return concatenated.substring(1, concatenated.length() - 1).contains(s);
+    }
+}
